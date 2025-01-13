783a04e21a9b5aea7084c986d9f5595cc3cfb241
Momen <touristmomen@gmail.com>
2025-01-13 14:18:37 +0600

    about updated

--- src/app/(withcommonlayout)/about/page.tsx
+++ src/app/(withcommonlayout)/about/page.tsx
@@ -8,7 +8,6 @@ const About = () => {
     <div>
       <section
         id="about"
-        // className="min-h-screen"
         style={{
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://images.unsplash.com/photo-1678247539441-05ad26a18343?q=80&w=2128&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
           backgroundSize: "cover",
