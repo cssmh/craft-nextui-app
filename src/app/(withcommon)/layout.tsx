c7b9310d2f92cffd2aef7f08c7bb81867d1302c4
Momen <touristmomen@gmail.com>
2025-01-14 00:07:57 +0600

    card music added, register route

--- src/app/(withcommon)/layout.tsx
+++ src/app/(withcommon)/layout.tsx
@@ -1,14 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { Navbar } from "@/src/components/navbar";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative flex flex-col h-screen">
       <Navbar />
-      <main 
-    //   className="container mx-auto max-w-7xl flex-grow"
-      >
-        {children}
-      </main>
+      <main>{children}</main>
     </div>
   );
 }
---
+++ src/app/register/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+const RegisterPage = () => {
+    return (
+        <div>
+            This is register page
+        </div>
+    );
+};
+
+export default RegisterPage;
